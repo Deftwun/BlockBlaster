4456f0935cf9a78ea03f310eabadca38befc60ff
Deftwun <jwake315@gmail.com>
2015-07-29 01:21:58 -0400

    Title bar implemented

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
+  chrome.app.window.create('index.html', {
     'frame':'none',
     'innerBounds': {
       'width': 800,
-      'height': 600}
-    'resizable':false}
+      'height': 600},
+    'resizable':false})
 });
